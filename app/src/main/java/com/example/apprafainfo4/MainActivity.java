da1ee25281d338f18b6dc234f19d9f8a3fe8ccce
rafa1 <rafa10_nogueira@hotmail.com>
2022-02-23 13:31:18 -0300

    Commit 13-31 23/02

--- app/src/main/java/com/example/apprafainfo4/MainActivity.java
+++ app/src/main/java/com/example/apprafainfo4/MainActivity.java
@@ -9,13 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText edit;
     private Button button;
-    private TextView mostra;
+    private String mostra = "teste";
     private Editable varS;
-    private  EditText phoneEdit;
+    private  EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +24,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         edit = (EditText) findViewById(R.id.nomeEdit);
-        mostra = (TextView) findViewById(R.id.mostraNome);
-        phoneEdit = (EditText) findViewById(R.id.phoneEdit);
+        senha = (EditText) findViewById(R.id.phoneEdit);
         Button button = (Button) findViewById(R.id.enviar);
-
+//        Toast toast = Toast.makeText(context, text, duration);
        button.setOnClickListener(this);
+
 
     }
     @Override
     public  void onClick(View view){
-        Bundle b  = new Bundle();
-        Intent janela = new Intent(this,Activity2.class);
-        varS = edit.getText();
-        b.putString("nome", String.valueOf(varS));
-        janela.putExtras(b);
-        startActivity(janela);
-//        mostra.setText(edit.getText());
+        Editable nome = edit.getText();
+        Editable passwords = senha.getText();
+        if(String.valueOf(nome) == "Rafael" && String.valueOf(passwords) == "123"){
+            Bundle b  = new Bundle();
+            Intent janela = new Intent(this,Activity2.class);
+            varS = edit.getText();
+            b.putString("nome", String.valueOf(varS));
+            janela.putExtras(b);
+            startActivity(janela);
+        }else{
+            Toast mensagem = Toast.makeText(this,mostra,Toast.LENGTH_SHORT);
+            mensagem.show();
+        }
+
+
     }
 }
